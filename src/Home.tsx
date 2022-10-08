8775c3a0266b92ac636df53cad8a050ebb69e624
Leonid Belyaev <l.belyaev@vk.team>
2022-10-08 12:12:45 +0300

    Removed VKUI

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,33 +1,13 @@
-import {
-  Group,
-  Panel,
-  PanelHeader,
-  SimpleCell,
-  SplitCol,
-  SplitLayout,
-  useAdaptivity,
-  View,
-  ViewWidth,
-  Header
-} from '@vkontakte/vkui';
 import React from 'react';
+import { Info } from './Info';
+import { MyComponent } from './MyComponent/MyComponent';
 
 export const Home: React.FC = () => {
-  const { viewWidth } = useAdaptivity();
   return (
-    <SplitLayout header={<PanelHeader separator={false} />}>
-      <SplitCol spaced={viewWidth && viewWidth > ViewWidth.MOBILE}>
-        <View activePanel="main">
-          <Panel id="main">
-            <PanelHeader>VKUI</PanelHeader>
-            <Group header={<Header mode="secondary">Items</Header>}>
-              <a href="/about">
-                <SimpleCell>Go to spinner</SimpleCell>
-              </a>
-            </Group>
-          </Panel>
-        </View>
-      </SplitCol>
-    </SplitLayout>
+    <>
+      <div>test</div>
+      <Info text="TEST"/>
+      <MyComponent text="KEK ))))"/>
+    </>
   );
 };
