a50ab6cd1bea810ba57ead9253a62c376e814ddb
Leonid Belyaev <l.belyaev@vk.team>
2022-10-08 16:07:50 +0300

    Added timer

--- src/Timer/Timer.tsx
+++ src/Timer/Timer.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import './Timer.css';
+import React, { useState } from 'react';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
+
+import styles from './Timer.module.css';
+import { Button } from 'react-bootstrap';
 
 const Timer: React.FC = () => {
-  return(
-    <div>Таймер</div>
+  const [duration, setDuration] = useState<number>(0);
+  return(<>
+    <div className={styles.timerContainer}>
+      <CountdownCircleTimer
+        isPlaying
+        duration={duration}
+        colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+        colorsTime={[7, 5, 2, 0]}
+      >
+        {({ remainingTime }) => remainingTime}
+      </CountdownCircleTimer>
+    </div>
+    <Button>Установить</Button>
+  </>
   );
 };
 
