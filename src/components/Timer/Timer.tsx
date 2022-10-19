f991d5945c3ee56bd35d1bbe850b97ba0b094073
Leonid Belyaev <l.belyaev@vk.team>
2022-10-19 16:32:16 +0300

    Звук имеетьбся

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 // https://www.npmjs.com/package/react-countdown-circle-timer
 import { CountdownCircleTimer } from 'react-countdown-circle-timer';
+import { Howl } from 'howler';
 
 import styles from './Timer.module.css';
 import { Button, Table } from 'react-bootstrap';
 import { Pause, Play, Stop } from 'react-bootstrap-icons';
+import formatDuration from 'format-duration';
 
 export interface TimerProps {
   id: string;
   remainingTime?: number;
 }
+
+const FINISHED_FADE_TIME = 7000;
 
 const Timer: React.FC<TimerProps> = ({ id, remainingTime = 0 }) => {
   const [userDefinedDuration, setUserDefinedDuration] = useState<number>(0);
@@ -17,8 +21,18 @@ const Timer: React.FC<TimerProps> = ({ id, remainingTime = 0 }) => {
   const [key, setKey] = useState<number>(0);
   const [isPlaying, setIsPlaying] = useState<boolean>(true);
   const [paused, setIsPaused] = useState<boolean>(false);
+  // const [playSound, exposedData] = useSound('../../../assets/tick.opus');
+  const [tick] = useState(new Howl({
+    src: ['../../../assets/tick.opus'],
+    loop: true,
+  }));
+  const [finished] = useState(new Howl({
+    src: ['../../../assets/finished.opus'],
+    loop: true,
+  }));
 
   const playClicked = () => {
+    tick.play();
     if (paused) {
       setIsPaused(false);
       return setIsPlaying(true);
@@ -31,11 +45,14 @@ const Timer: React.FC<TimerProps> = ({ id, remainingTime = 0 }) => {
   };
 
   const pauseClicked = () => {
+    tick.stop();
     setIsPlaying(false);
     setIsPaused(true);
   };
 
   const stopClicked = () => {
+    tick.stop();
+    finished.stop();
     setIsPlaying(false);
     setIsPaused(false);
     setDuration(0);
@@ -44,13 +61,18 @@ const Timer: React.FC<TimerProps> = ({ id, remainingTime = 0 }) => {
 
   const startTimer = (e: any) => {
     const stringTime = e.currentTarget.innerHTML;
-    const seconds = stringTime.split(':').reduce((res: number, time: string) => {
-      return res * 60 + parseInt(time);
-    }, 0);
+    const seconds = stringTime.split(':').reduce((res: number, time: string) => res * 60 + parseInt(time), 0);
     setDuration(seconds);
     setIsPlaying(true);
     setIsPaused(false);
     setKey(key+1);
+  };
+
+  const timerCompleteHandler = () => {
+    tick.stop();
+    finished.play();
+    finished.fade(1, 0, FINISHED_FADE_TIME);
+    setTimeout(() => finished.stop(), FINISHED_FADE_TIME);
   };
 
   return(
@@ -70,11 +92,12 @@ const Timer: React.FC<TimerProps> = ({ id, remainingTime = 0 }) => {
         <CountdownCircleTimer
           key={key}
           isPlaying={isPlaying}
+          onComplete={timerCompleteHandler}
           duration={duration}
           colors={['#004777', '#F7B801', '#A30000', '#A30000']}
           colorsTime={[7, 5, 2, 0]}
         >
-          {({ remainingTime }) => remainingTime}
+          {({ remainingTime }) => formatDuration(remainingTime * 1000)}
         </CountdownCircleTimer>
         <form className={styles.setTimeForm}>
           <input
